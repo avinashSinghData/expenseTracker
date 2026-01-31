6afa966f625d7fbf7b203b4d987c54bc8441759b
HawkWolf1 <singhavinash9fg@gmail.com>
2026-02-01 00:55:11 +0530

    updated datasbase's variables

--- util/database.js
+++ util/database.js
@@ -2,9 +2,19 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config() 
 
-const sequelize = new Sequelize('practise', 'root', 'Bucketone23@', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
+//  const sequelize = new Sequelize('practise', 'root', 'Bucketone23@', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// })
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD
+    {
+        host: process.env.DB_HOST,
+        dialect: "postgres"
+    }
+)
 
 module.exports = sequelize
